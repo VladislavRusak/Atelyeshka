fd61a4bcd0d8eaf860968d38eeaf5d3d61f523e3
Vladislav Rusakov <vladislav.west01@gmail.com>
2022-11-07 16:51:34 +0300

    add the check function for database

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import sys
+import os
+import sqlite3
 
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel, QSqlQueryModel
 from window import *
 
-from db.service import service
+
+try:
+    sqlite_connection = sqlite3.connect('db/Alelye.sqlite3')
+    cursor = sqlite_connection.cursor()
+    print("База данных успешно подключена")
+
+    cursor.execute("select sqlite_version();")
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Ошибка при подключении к SQLite", error)
+finally:
+    if (sqlite_connection):
+        sqlite_connection.close()
+        print("Соединение с SQLite закрыто.")
 
 
 if __name__ == '__main__':
